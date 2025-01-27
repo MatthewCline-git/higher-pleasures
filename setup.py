b53888a156d000cfec36c4874ef4a14bd822fc0e
MatthewCline-git <matthewcline1028@gmail.com>
2025-01-26 22:06:56 -0500

    Modify setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ setup(
     name="higher_pleasures",
     version="0.1.0",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         line.strip()
         for line in open("requirements.txt")
         if line.strip() and not line.startswith("#")
     ],
+    python_requires=">=3.11",
 )
