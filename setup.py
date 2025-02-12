69144a10a1a4480df573aad9672e83a556619a2c
MatthewCline-git <matthewcline1028@gmail.com>
2025-02-11 20:45:15 -0500

    Pass linter
    changed name of logging/ directory because it shadows python's
    logging library

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
 
@@ -6,6 +8,8 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[line.strip() for line in open("requirements.txt") if line.strip() and not line.startswith("#")],
+    install_requires=[
+        line.strip() for line in Path.open("requirements.txt") if line.strip() and not line.startswith("#")
+    ],
     python_requires=">=3.11",
 )
