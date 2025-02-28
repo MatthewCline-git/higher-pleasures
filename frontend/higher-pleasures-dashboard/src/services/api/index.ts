35006a33c381512d1f31846da0c4b8873cd9875d
MatthewCline-git <matthewcline1028@gmail.com>
2025-02-28 13:03:58 -0500

    Make skeleton user entries API

---
+++ frontend/higher-pleasures-dashboard/src/services/api/index.ts
@@ -0,0 +1,4 @@
+import apiClient from "./client";
+import userStatsService from "./userStats";
+
+export { apiClient, userStatsService };
---
+++ frontend/higher-pleasures-dashboard/src/services/api/userStats.ts
@@ -0,0 +1,17 @@
+import apiClient from "./client";
+
+export interface Entry {
+  user_id: string;
+  user_activity_id: number;
+  date: Date;
+  duration_minutes: number;
+  raw_input: string;
+}
+
+const userStatsService = {
+  getUserEntries: async (userId: string): Promise<Entry[]> => {
+    return apiClient.get(`/db/${userId}/entries`);
+  },
+};
+
+export default userStatsService;
